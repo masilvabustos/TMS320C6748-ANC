9139a167a2eaca316a4ef32c580a2c61d7f24544
Santiago <Santiago@Santiago-PC>
2017-04-11 18:52:24 -0300

    Configurar la salida a pull-down y agregar un delay.

--- sw/main.c
+++ sw/main.c
@@ -19,7 +19,7 @@ int main(void)
     GPIOBank6Pin12PinMuxSetup();
 
     PSCModuleControl(SOC_PSC_1_REGS, HW_PSC_GPIO, PSC_POWERDOMAIN_ALWAYS_ON, PSC_MDCTL_NEXT_ENABLE);
-
+    * (volatile unsigned int*) 0x01E2C010 = 0;   // Coloca la salida de los LEDs en Pull Down
     GPIODirModeSet(SOC_GPIO_0_REGS, GPIO_PIN(6, 12), GPIO_DIR_OUTPUT);
 
     while(1) {
@@ -27,7 +27,7 @@ int main(void)
         GPIOPinWrite(SOC_GPIO_0_REGS, GPIO_PIN(6, 12), GPIO_PIN_HIGH);
         delay(1000000);
         GPIOPinWrite(SOC_GPIO_0_REGS, GPIO_PIN(6, 12), GPIO_PIN_LOW);
-
+        delay(1000000);
     }
 	
 	return 0;
